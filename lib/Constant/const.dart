55a2a28662fbf7fcc0337ba700b2be74278a33a5
fauko09 <78807088+fauko09@users.noreply.github.com>
2021-08-09 20:28:14 +0700

    flutter

---
+++ lib/Constant/const.dart
@@ -0,0 +1,13 @@
+String url = "http://vigenesia.org/";
+// String url = "http://203.161.184.124/";
+// String url = "http://192.168.1.9";
+// String url = 'http://192.168.1.4';
+
+
+
+
+
+
+
+
+// Change This For Different IP
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'Screens/Login.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:
+          Login(), // <-- Buat Class Baru yg bernama MyScreen di dalam lib bikin folder baru screens isinya MyScreen.dart
+    ));
